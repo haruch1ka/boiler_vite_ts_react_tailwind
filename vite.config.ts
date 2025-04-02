6527efcdcf7b66aa10a431db919c55c5ecd46dea
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-04-02 17:19:01 +0900

    Viteの設定ファイルで、baseパスを'./'に変更しました。

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   root: "src",
-  base: "/",
+  base: "./",
   build: {
     outDir: "../dist",
   },
