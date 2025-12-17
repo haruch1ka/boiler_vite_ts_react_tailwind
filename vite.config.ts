e3c7fb9c1bf4c524746b713b0e357f8caf15af0f
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-12-17 13:18:09 +0900

    feat: .envファイルのビルド日付を更新し、vite-plugin-static-copyを追加しました。また、vite.svgを削除し、vite.config.tsを修正しました

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -7,7 +8,8 @@ export default defineConfig(({ mode }) => {
   const buildName = env.VITE_BUILD_NAME || "myApp";
   const htlmlPlugin = () => ({
     name: "html-transform",
-    transformIndexHtml: (html: string): string => html.replace(/%=(.*?)%/g, (match, p1) => env[p1] ?? match),
+    transformIndexHtml: (html: string): string =>
+      html.replace(/%=(.*?)%/g, (match, p1) => env[p1] ?? match),
   });
   return {
     root: "src",
@@ -20,7 +22,18 @@ export default defineConfig(({ mode }) => {
       outDir: `./../dist/${buildName}/`,
       emptyOutDir: true,
     },
-    plugins: [htlmlPlugin(), react()],
+    plugins: [
+      htlmlPlugin(),
+      react(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: "./assets/*",
+            dest: "./assets",
+          },
+        ],
+      }),
+    ],
     publicDir: "../public",
   };
 });
