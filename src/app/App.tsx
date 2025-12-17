303d4630727375d29dde5247248ab91d4ce5e63d
d-ito1106 <d_ito@taiho-pp.co.jp>
2025-12-17 13:45:52 +0900

    feat: アプリの構造を再編成し、コンポーネントを新規作成しました。App.tsxを作成し、Topコンポーネントを追加しました。また、ヘッダー、フッター、メインコンテンツのコンポーネントを実装しました。index.htmlを更新し、スクリプトのパスを修正しました。

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import Top from "../components/page/Top/Top.tsx";
+import "../styles/tailwind.css";
+
+function App() {
+  return (
+    <>
+      <div className="h-full w-full bg-transparent">
+        <Top />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/page/Top/Top.tsx
@@ -0,0 +1,14 @@
+import AppContents from "./AppContents.js";
+import AppHeader from "./AppHeader.js";
+import AppFooter from "./AppFooter.js";
+
+function Top() {
+  return (
+    <>
+      <AppHeader />
+      <AppContents />
+      <AppFooter />
+    </>
+  );
+}
+export default Top;
